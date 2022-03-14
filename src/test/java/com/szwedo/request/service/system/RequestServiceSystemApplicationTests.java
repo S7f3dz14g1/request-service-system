52f8f94f6adf878467a37ed45ebeeac74f30330e
Szfeduś <krystianszwedo@wp.pl>
2022-03-14 19:49:13 +0100

    added empty project

---
+++ src/test/java/com/szwedo/request/service/system/RequestServiceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szwedo.request.service.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestServiceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
